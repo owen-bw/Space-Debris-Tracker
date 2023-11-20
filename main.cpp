247601c29af5efd0913c272afbf6d2c7fbfaff70
Blake <epicbwo@gmail.com>
2023-11-19 23:49:33 -0500

    Added event loop

--- main.cpp
+++ main.cpp
@@ -9,11 +9,26 @@ int main(int argc, char* argv[]) {
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-	SDL_RenderClear(renderer);
+	bool running = true;
+    SDL_Event event;
 
-	SDL_RenderPresent(renderer);
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
 
-	SDL_Delay(10000);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
 	return 0;
 }
