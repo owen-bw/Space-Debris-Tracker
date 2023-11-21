891db01aebe23702f89103cba78b55c3e7ccef07
ararity <epicbwo@gmail.com>
2023-11-21 18:08:51 -0500

    Windows cmake changes and lib

--- build/CMakeFiles/space-debris-tracker.dir/DependInfo.cmake
+++ build/CMakeFiles/space-debris-tracker.dir/DependInfo.cmake
@@ -8,21 +8,21 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/gl.c" "CMakeFiles/space-debris-tracker.dir/src/gl.c.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/gl.c.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/BitmapFontData.cpp" "CMakeFiles/space-debris-tracker.dir/src/BitmapFontData.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/BitmapFontData.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/Bmp.cpp" "CMakeFiles/space-debris-tracker.dir/src/Bmp.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Bmp.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/Matrices.cpp" "CMakeFiles/space-debris-tracker.dir/src/Matrices.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Matrices.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/Sphere.cpp" "CMakeFiles/space-debris-tracker.dir/src/Sphere.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Sphere.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/Timer.cpp" "CMakeFiles/space-debris-tracker.dir/src/Timer.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Timer.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/Tokenizer.cpp" "CMakeFiles/space-debris-tracker.dir/src/Tokenizer.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Tokenizer.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui_demo.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_demo.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_demo.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui_draw.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_draw.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_draw.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui_impl_glfw.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_glfw.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui_impl_opengl3.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_opengl3.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui_tables.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_tables.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_tables.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/imgui_widgets.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_widgets.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_widgets.cpp.o.d"
-  "/Users/blake/projects/cpp/COP3530/Project 3/Space-Debris-Tracker/src/main.cpp" "CMakeFiles/space-debris-tracker.dir/src/main.cpp.o" "gcc" "CMakeFiles/space-debris-tracker.dir/src/main.cpp.o.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/gl.c" "CMakeFiles/space-debris-tracker.dir/src/gl.c.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/gl.c.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/BitmapFontData.cpp" "CMakeFiles/space-debris-tracker.dir/src/BitmapFontData.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/BitmapFontData.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/Bmp.cpp" "CMakeFiles/space-debris-tracker.dir/src/Bmp.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Bmp.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/Matrices.cpp" "CMakeFiles/space-debris-tracker.dir/src/Matrices.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Matrices.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/Sphere.cpp" "CMakeFiles/space-debris-tracker.dir/src/Sphere.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Sphere.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/Timer.cpp" "CMakeFiles/space-debris-tracker.dir/src/Timer.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Timer.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/Tokenizer.cpp" "CMakeFiles/space-debris-tracker.dir/src/Tokenizer.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/Tokenizer.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui_demo.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_demo.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_demo.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui_draw.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_draw.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_draw.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui_impl_glfw.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_glfw.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui_impl_opengl3.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_impl_opengl3.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui_tables.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_tables.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_tables.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/imgui_widgets.cpp" "CMakeFiles/space-debris-tracker.dir/src/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/imgui_widgets.cpp.obj.d"
+  "D:/C++/COP3530/Project 3/SpaceDebrisTracker/src/main.cpp" "CMakeFiles/space-debris-tracker.dir/src/main.cpp.obj" "gcc" "CMakeFiles/space-debris-tracker.dir/src/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- src/imgui_impl_glfw.cpp
+++ src/imgui_impl_glfw.cpp
@@ -85,7 +85,7 @@
 #ifdef _WIN32
 #undef APIENTRY
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>   // for glfwGetWin32Window()
+#include <glfw3native.h>   // for glfwGetWin32Window()
 #endif
 #ifdef __APPLE__
 #define GLFW_EXPOSE_NATIVE_COCOA
